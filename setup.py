5a47b3d6a871db9d5397d6537bc58cbc126e2e5b
Sauci <guillaumesottas@gmail.com>
2022-12-08 15:32:52 +0100

    improve setup

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import os.path
+
 from setuptools import setup
+
+with open(os.path.abspath(os.path.join(os.path.dirname(__file__), 'requirements.txt')), 'r') as fp:
+    install_requires = fp.read().splitlines()
+
+with open(os.path.abspath(os.path.join(os.path.dirname(__file__), 'README.md')), 'r') as fp:
+    long_description = fp.read()
 
 setup(
     name='bsw_code_gen',
@@ -9,9 +17,9 @@ setup(
     author='Guillaume Sottas',
     author_email='guillaumesottas@gmail.com',
     description='Generic code generator used to generate C sources and headers of BSW modules',
+    long_description=long_description,
     entry_points={
         'console_scripts': ['bsw_code_gen=bsw_code_gen:main']
     },
-    install_requires=['jinja2',
-                      'jsonschema'],
+    install_requires=install_requires,
 )
